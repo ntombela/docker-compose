307d827baa42b809b26dc04029fa35404845a289
Gugulethu Ntombela <gugulethun@gmail.com>
2020-05-03 14:47:41 +0200

    Initial commit..

---
+++ client/Ntombela.InventoryTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ntombela.InventoryTracker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
